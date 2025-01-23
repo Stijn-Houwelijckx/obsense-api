5e48ad5c3cb3e9d2892b1dce51d468e817908cc3
Stijn Houwelijckx <stijn@houwelijckx.be>
2025-01-24 00:54:57 +0100

    feat: add index API route to get all users (#7)

--- controllers/api/v1/users.js
+++ controllers/api/v1/users.js
@@ -1,3 +1,34 @@
 const User = require("../../../models/api/v1/User");
 
-module.exports = {};
+// Get all users
+const index = async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(204).json({
+        status: "success",
+        message: "No users found",
+        data: {
+          users: [],
+        },
+      });
+    } else {
+      res.status(200).json({
+        status: "success",
+        data: {
+          users: users,
+        },
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: "Unable to fetch users",
+      error: err.message,
+    });
+  }
+};
+
+module.exports = {
+  index,
+};
--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const userController = require("../../../controllers/api/v1/users");
 const authController = require("../../../controllers/auth");
 const passport = require("../../../passport/passport");
+
+router.get("/", userController.index);
 
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
