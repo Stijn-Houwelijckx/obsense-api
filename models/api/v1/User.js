03e174930189bfcdecff4656b9850cffc3805c3a
Stijn Houwelijckx <stijn@houwelijckx.be>
2025-02-03 23:37:08 +0100

    feat: add profilePicture field to UserSchema

--- models/api/v1/User.js
+++ models/api/v1/User.js
@@ -9,6 +9,12 @@ const UserSchema = new Schema(
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     isArtist: { type: Boolean, default: false },
+    profilePicture: {
+      fileName: { type: String },
+      filePath: { type: String },
+      fileType: { type: String },
+      fileSize: { type: Number },
+    },
   },
   { timestamps: true } // Automatically add createdAt and updatedAt fields
 );
