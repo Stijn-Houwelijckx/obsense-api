efc7e1f3890b0e3d39290be0940ca827b803006c
Stijn Houwelijckx <stijn@houwelijckx.be>
2025-02-15 19:36:26 +0100

    fix: make likes and views arrays

--- models/api/v1/Collection.js
+++ models/api/v1/Collection.js
@@ -23,19 +23,22 @@ const CollectionSchema = new Schema(
     ], // Placed 3D objects
     maxObjects: { type: Number, default: 10 },
     timesBought: { type: Number, default: 0 },
-    likes: { type: Schema.Types.ObjectId, ref: "User", default: [] },
-    views: { type: Schema.Types.ObjectId, ref: "User", default: [] },
-    ratings: [
-      {
-        user: { type: Schema.Types.ObjectId, ref: "User" },
-        rating: {
-          type: Number,
-          min: 0,
-          max: 5,
-          default: 0,
+    likes: [{ type: Schema.Types.ObjectId, ref: "User", default: [] }],
+    views: [{ type: Schema.Types.ObjectId, ref: "User", default: [] }],
+    ratings: {
+      type: [
+        {
+          user: { type: Schema.Types.ObjectId, ref: "User" },
+          rating: {
+            type: Number,
+            min: 0,
+            max: 5,
+            default: 0,
+          },
         },
-      },
-    ],
+      ],
+      default: [], // Default to an empty array
+    },
     genres: [{ type: Schema.Types.ObjectId, ref: "Genre", default: [] }],
     location: {
       lat: { type: Number, default: null },
