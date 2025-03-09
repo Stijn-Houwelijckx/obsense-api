4969a85c99aefc17b52a4059410d9a350f8f5fe2
Stijn Houwelijckx <stijn@houwelijckx.be>
2025-03-09 21:44:04 +0100

    feat: add indexByCreator function to get all collections from a creator (#22)

--- controllers/api/v1/collections.js
+++ controllers/api/v1/collections.js
@@ -4,7 +4,7 @@ const User = require("../../../models/api/v1/User");
 // Get all collections
 const index = async (req, res) => {
   try {
-    // Check if the user is authenticated and is an artist
+    // Check if the user is authenticated
     if (!req.user) {
       return res.status(401).json({
         status: "fail",
@@ -65,6 +65,74 @@ const index = async (req, res) => {
   }
 };
 
+// Get all collections by a specific artist
+const indexByCreator = async (req, res) => {
+  try {
+    // Check if the user is authenticated
+    if (!req.user) {
+      return res.status(401).json({
+        status: "fail",
+        data: {
+          message: "Unauthorized",
+        },
+      });
+    }
+
+    const creatorId = req.params.id;
+
+    // Fetch only published and active collections
+    const collections = await Collection.find({
+      createdBy: creatorId,
+      isPublished: true,
+      isActive: true,
+    })
+      .select(
+        "_id type title price coverImage createdBy likes views ratings location"
+      )
+      .populate("createdBy", "username");
+
+    if (!collections || collections.length === 0) {
+      return res.status(404).json({
+        status: "fail",
+        data: {
+          message: "No collections found.",
+        },
+      });
+    }
+
+    // Process collections to add the number of likes, views, and average rating
+    const processedCollections = collections.map((collection) => {
+      const processedCollection = collection.toObject();
+      processedCollection.likes = collection.likes.length;
+      processedCollection.views = collection.views.length;
+      // Calculate the average rating
+      if (collection.ratings.length > 0) {
+        const totalRating = collection.ratings.reduce((acc, rating) => {
+          return acc + rating.rating;
+        }, 0);
+        processedCollection.ratings = totalRating / collection.ratings.length;
+      } else {
+        processedCollection.ratings = 0;
+      }
+      return processedCollection;
+    });
+
+    return res.status(200).json({
+      status: "success",
+      data: { collections: processedCollections },
+    });
+  } catch (error) {
+    console.error("Error fetching collections:", error);
+    return res.status(500).json({
+      status: "fail",
+      data: {
+        message: "Error fetching collections.",
+      },
+    });
+  }
+};
+
 module.exports = {
   index,
+  indexByCreator,
 };
--- routes/api/v1/collections.js
+++ routes/api/v1/collections.js
@@ -3,11 +3,18 @@ const router = express.Router();
 const passport = require("../../../passport/passport");
 const collectionController = require("../../../controllers/api/v1/collections");
 
-// Route to get collections for the logged-in artist
+// Route to get all collections
 router.get(
   "/",
   passport.authenticate("jwt", { session: false }), // Ensure the user is authenticated
   collectionController.index // Controller to get the artist's collections
+);
+
+// Route to get all collections by artist ID
+router.get(
+  "/creator/:id",
+  passport.authenticate("jwt", { session: false }), // Ensure the user is authenticated
+  collectionController.indexByCreator // Controller to get a single collection
 );
 
 module.exports = router;
